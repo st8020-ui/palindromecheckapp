e25f1ad04d6c3c091aea788c8c536d04b148b6aa
st8020-ui <st8020@srmist.edu.in>
2026-02-26 09:32:26 +0530

    UC4 COMPLETE

--- palindromecheck/src/Palindromecheck.java
+++ palindromecheck/src/Palindromecheck.java
@@ -1,18 +1,27 @@
 public class Palindromecheck {
     public static void main (String[] args) {
 
-                String original = "level";
-                String reversed = "";
+        String input = "radar";
+        char[] characters = input.toCharArray();
 
-                for (int i = original.length() - 1; i >= 0; i--) {
-                    reversed = reversed + original.charAt(i);
-                }
+        int start = 0;
+        int end = characters.length - 1;
+        boolean isPalindrome = true;
 
-                if (original.equals(reversed)) {
-                    System.out.println(original + " is a Palindrome.");
-                } else {
-                    System.out.println(original + " is NOT a Palindrome.");
-                }
+        while (start < end) {
+            if (characters[start] != characters[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome.");
+        } else {
+            System.out.println(input + " is NOT a Palindrome.");
+        }
             }
         }
 
